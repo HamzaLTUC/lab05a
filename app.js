5dee6694eebd4d14eab9924a655c96438817c2ec
Hamza Qahoush <haamza.qah@gmail.com>
2021-03-05 14:52:24 +0200

    problem 2 solved

--- app.js
+++ app.js
@@ -25,16 +25,18 @@ function sum(a, b) { //eslint-disable-line
         Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
         
         "The product of 5 and 9 is 45."
-        
+        "The product of 5 and 9 is 45."
         Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
         // Write your code here
         function multiply(a, b) { //eslint-disable-line
-
+            let equation = a*b
+            let express = 'The product of' +' '+ a +' '+ 'and'+' '+ b +' '+ 'is' +' '+ equation+'.'
+            return [equation,express] ;
         }
 
-        // Here is the test for multiply(); uncomment it to run it
-        // testMultiply(5,9);
+    multiply();
+ testMultiply(5,9);
 
         // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
